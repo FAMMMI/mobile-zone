f3e45f60646138c59e3bb5aed8bd32a0945547e5
FAMMMI <naimfami@gmail.com>
2022-03-01 16:41:57 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 
+// fetching mobile list from api
 const searchMobile = () => {
-    console.log('clicked');
+    // console.log('clicked');
     document.getElementById('search-result').innerHTML = "";
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    toggleSpinner('block');
+    singledetails('none');
+    displayError2('none');
+    showmore('none');
     searchField.value = '';
     if (searchText == '') {
-        displayError('block');
+        displayError2('block');
+        displayError('none');
+        toggleSpinner('none');
+        showmore('none');
+
     }
     else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -16,44 +25,77 @@ const searchMobile = () => {
                 console.log(data);
                 if (data.status == false) {
                     displayError('block');
+                    displayError2('none');
+                    toggleSpinner('none');
+                    showmore('none');
 
                 }
                 else {
-                    displaySearchReslt(data.data);
+                    displaySearchResult(data.data);
                     displayError('none');
+                    displayError2('none');
                 }
             })
             .catch(error => displayError(error));
+        displayError('none');
+        displayError2('none');
     }
+
 }
 // searchMobile();
-
+// error message
 const displayError = error => {
     document.getElementById('error-message').style.display = error;
 }
+const displayError2 = error => {
+    document.getElementById('error-message-2').style.display = error;
+}
+// spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+// blocking single details
+const singledetails = displayStyle => {
+    document.getElementById('singleDetails').style.display = displayStyle;
+}
+// show more button showing
+const showmore = displayStyle => {
+    document.getElementById('show-more').style.display = displayStyle;
+}
 
-
-const displaySearchReslt = (mobiles) => {
+// display search results
+const displaySearchResult = (mobiles) => {
     const searchResult = document.getElementById('search-result');
     // searchResult.textContent = '';
     console.log(mobiles.slice(0, 20));
-    mobiles.slice(0, 20).forEach(mobile => {
+    mobiles.splice(0, 20).forEach(mobile => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100 pt-3">
+        <div class="card bg-light h-100 pt-3 mb-3">
             <img src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="...">
-                <div class="card-body text-center">
+                <div class="card-body text-center ">
                     <h5 class="card-title">Name: ${mobile.phone_name}</h5>
                     <h5 class="card-text">Brand: ${mobile.brand}</h5>
-                    <button onclick="loadMobileDetail('${mobile.slug}')" class=" text-center">details</button>
+                    
+                    <button onclick="loadMobileDetail('${mobile.slug}')" class=" text-center button">details</button>
                 </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+
+    toggleSpinner('none');
+    singledetails('none');
+    showmore('none');
+
+
+
+
 }
 
+
+// fetching mobile details by id
 const loadMobileDetail = (mobileId) => {
     console.log(mobileId);
     const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
@@ -62,8 +104,9 @@ const loadMobileDetail = (mobileId) => {
         .then(data => showDataDetails(data.data));
 }
 
+// showing mobile details
 const showDataDetails = (mobile) => {
-    console.log(mobile);
+    // console.log(mobile);
     const singleDetail_Id = document.getElementById('singleDetails');
     singleDetail_Id.innerHTML = "";
     let div = document.createElement('div');
@@ -72,36 +115,42 @@ const showDataDetails = (mobile) => {
     for (let i = 0; i < mobile.mainFeatures.sensors.length; i++) {
         if (i == mobile.mainFeatures.sensors.length - 1) {
             sensors += mobile.mainFeatures.sensors[i];
+
             continue;
+
         }
         sensors += mobile.mainFeatures.sensors[i] + ", ";
     }
-    console.log(sensors);
+    // console.log(sensors);
     if (mobile.hasOwnProperty('releaseDate') && mobile.releaseDate != "") release_date += mobile.releaseDate;
-    else release_date += "Release Date Not published";
+    else release_date += "Release Date is Not published";
 
     for (const property in mobile.others) {
         // console.log(${property}: ${data.others[property]});
         otherFeatures += property + ": " + mobile.others[property] + ", "
     }
     if (otherFeatures == "") {
-        otherFeatures += "Other features not provoded yet."
+        otherFeatures += "Other features are not provoded yet."
     }
     div.innerHTML = `
-                <div class="card align-items-center p-2 w-75 mx-auto">
+                <div class="card align-items-center p-2 w-50 mx-auto bg-light">
                     <img src="${mobile.image}" class="card-img-top w-25 mx-auto" alt="...">
-                    <div class="card-body mx-auto">
+                    <div class="card-body mx-auto w-75">
                         <h3 class="card-title">Name: ${mobile.name}</h3>
                         <h4 class="card-title">Brand: ${mobile.brand}</h4>
                         <p class="card-text"><b>Storage:</b> ${mobile.mainFeatures.storage}</p>
                         <p class="card-text"><b>Display Size:</b> ${mobile.mainFeatures.displaySize}</p>
                         <p class="card-text"><b>Chipset:</b> ${mobile.mainFeatures.chipSet}</p>
                         <p class="card-text"><b>Memory:</b> ${mobile.mainFeatures.memory}</p>
-                        <p class="card-text"><b>Sensors:</b> ${sensors}</p>
+                        <p class="card-text"><b>Sensors:</b> '${sensors}'</p>
                         <p class="card-text"><b>Release Data:</b> ${release_date}</p>
-                        <p class="card-text"><b>Other Features:</b> ${otherFeatures}</p>
+                        <p class="card-text"><b>Other Features:</b> <br>
+                        ${otherFeatures} .<br>
+                        
+                        </p>
                     </div>
                 </div>
         `;
     singleDetail_Id.appendChild(div);
+    singledetails('block');
 }
